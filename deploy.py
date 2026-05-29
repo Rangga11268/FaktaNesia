a1b31344b8b7419fb9c9fa253db0865693819c7e
Darell Rangga <darrelrangga@gmail.com>
2026-05-29 13:28:25 +0700

    chore: update deploy.py to load env and upload credibility_manager.py

--- deploy.py
+++ deploy.py
@@ -1,22 +1,33 @@
 from huggingface_hub import HfApi
-api = HfApi()
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api = HfApi()
 token = os.getenv("HF_TOKEN")
 repo_id = "darell123/faktanesia-backend"
 
 try:
-    print("Uploading model...")
-    api.upload_file(
-        path_or_fileobj="model/hoax_model.pkl",
-        path_in_repo="model/hoax_model.pkl",
-        repo_id=repo_id,
-        repo_type="space",
-        token=token
-    )
+#    print("Uploading model...")
+#    api.upload_file(
+#        path_or_fileobj="model/hoax_model.pkl",
+#        path_in_repo="model/hoax_model.pkl",
+#        repo_id=repo_id,
+#        repo_type="space",
+#        token=token
+#    )
     print("Uploading app.py...")
     api.upload_file(
         path_or_fileobj="app.py",
         path_in_repo="app.py",
+        repo_id=repo_id,
+        repo_type="space",
+        token=token
+    )
+    print("Uploading credibility_manager.py...")
+    api.upload_file(
+        path_or_fileobj="credibility_manager.py",
+        path_in_repo="credibility_manager.py",
         repo_id=repo_id,
         repo_type="space",
         token=token
